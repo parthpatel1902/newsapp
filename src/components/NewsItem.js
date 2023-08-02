d2c6fc5ef002bae18beca22079f8ce1979cda1ca
Parth Patel <100956473+parthpatel1902@users.noreply.github.com>
2023-08-02 18:24:02 +0530

    change in newsitem

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,13 +4,13 @@ export default class NewsItem extends Component {
   render(props) {
     let {title,description,imgurl,newsurl} = this.props;
     return (
-      <div>
+      <div className='m-3'>
         <div className="card" style={{width: '18rem'}}>
             <img className='card-img-top' src={imgurl} alt='cricket1'/>
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href="/" className="btn btn-primary btn-block">Read More</a>
+              <a href={newsurl} target='blank' className="btn btn-primary btn-block">Read More</a>
             </div>
         </div>
       </div>
